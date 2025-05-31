29067496c6a13f8023d063273b928c296bec82f4
Lucas Rocha <lucas.oct8@gmail.com>
2025-05-31 19:09:21 -0300

    Initial commit

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+public class hello_world
+{
+    public static void main (String args[])
+    {
+        print_hello();
+    }
+
+    private static void print_hello()
+    {
+        System.out.print("Hello world!");
+    }
+
+}
